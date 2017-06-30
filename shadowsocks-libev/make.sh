328a87f256bacffa61c685181cd7128446350b8c
sunliang <sunliang711@163.com>
2017-07-01 07:11:53 +0800

    小修改

--- shadowsocks-libev/make.sh
+++ shadowsocks-libev/make.sh
@@ -6,57 +6,56 @@ fi
 shopt -s expand_aliases
 if (("$proxy"==1));then
 	echo "Using proxy..."
-	git config --global http.proxy "192.168.1.120:1080"
-	git config --global https.proxy "192.168.1.120:1080"
-	alias curl='curl --socks5 "192.168.1.120:1080"'
+    socksproxy="socks5://10.0.0.1:23456"
+	git config --global http.proxy "$socksproxy"
+	git config --global https.proxy "$socksproxy"
+	alias curl="curl --socks5 $socksproxy"
 fi
 #sh -c 'printf "deb http://httpredir.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/jessie-backports.list'
 apt-get update -y
-
 apt install -y curl gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libudns-dev automake git
 
 ROOT=~/ss-libev
+sodiumVer=1.0.12
+mbedtlsVer=2.5.1
 rm -rf "$ROOT" >/dev/null 2>&1
 mkdir -pv "$ROOT"
 
 cd "$ROOT"
 
-git clone https://github.com/shadowsocks/shadowsocks-libev.git  
-cd shadowsocks-libev  
+#download shadowsocks-libev
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
+cd shadowsocks-libev
 git submodule update --init --recursive
 cd ..
 
+#remove libsodium
 apt-get remove libsodium* -y
-
 apt purge libsodium* -y
 
-curl  https://download.libsodium.org/libsodium/releases/libsodium-1.0.11.tar.gz -O
-
-tar xvf libsodium-1.0.11.tar.gz
-
-cd libsodium-1.0.11
-
+#compile libsodium
+curl  https://download.libsodium.org/libsodium/releases/libsodium-${sodiumVer}.tar.gz -O
+echo "extract libsodium..."
+tar xf libsodium-${sodiumVer}.tar.gz
+cd libsodium*
 ./configure --prefix=/usr && make
-
 make install
-
-cd ..
-
-curl  https://tls.mbed.org/download/mbedtls-2.4.0-gpl.tgz -O
-
-tar xvf mbedtls-2.4.0-gpl.tgz
-
-cd mbedtls-2.4.0
-
-make SHARED=1 CFLAGS=-fPIC
-
-make install
-
 ldconfig
 
 cd ..
 
-cd shadowsocks-libev  
+#compile mbedtls
+curl  https://tls.mbed.org/download/mbedtls-${mbedtlsVer}-gpl.tgz -O
+echo "extract mbedtls..."
+tar xf mbedtls*tgz
+cd mbedtls*
+make SHARED=1 CFLAGS=-fPIC
+make install
+ldconfig
+
+cd ..
+
+cd shadowsocks-libev
 ./autogen.sh && ./configure && make
 make install
 
